f90803795aaaba268c315336a72000bc2a16689b
Stefan Huber <sh@signalwerk.ch>
2019-02-18 13:22:41 +0100

    MOD: adjust some minor things

--- splitter.js
+++ splitter.js
@@ -1,71 +1,69 @@
-//   splittet doppelseiten in einzelseiten
-
+//   splits double page spreads in pdfs into single pages
 
 function splitterInfo() {
-    this.name = "splitter";
-    this.version = 0.5;
-    this.hauptmenue = this.name + " " + this.version;
-    this.info = this.name + " " + this.version + " || copyright by stefan huber || www.stefan-huber.ch";
- }
+  this.name = "splitter";
+  this.version = 0.8;
+  this.mainMenu = this.name + " " + this.version;
+  this.info = this.name + " " + this.version + " || Signalwerk GmbH";
+}
 
 var splitter = new splitterInfo();
 
+app.addSubMenu({ cName: splitter.mainMenu, cParent: "File" });
 
-app.addSubMenu({ cName: splitter.hauptmenue, cParent: "Tools" });
+app.addMenuItem({
+  cName: "Spreads to single pages",
+  cParent: splitter.mainMenu,
+  cExec: "app.alert(split(), 3);"
+});
 
-app.addMenuItem({ cName: "Doppelseiten in Einzelseiten", cParent: splitter.hauptmenue,
-cExec: "app.alert(split(), 3);"});
-
-app.addMenuItem({ cName: "Info", cParent: splitter.hauptmenue,
-cExec: "app.alert(splitter.info, 3);"});
+app.addMenuItem({
+  cName: "Info",
+  cParent: splitter.mainMenu,
+  cExec: "app.alert(splitter.info, 3);"
+});
 
 function split() {
+  // Specify cDefault only;
+  var firstStart = app.response("From which page on should we split?", "", 1);
+  var randpt = 42.52; //42.52 pt (15 mm)
 
-	var firstStart = app.response("Ab welcher Seite soll gesplittet werden?","", 1); // Specify cDefault only;
-	var randpt = 42.52; //42.52 pt (15 mm)
+  var firstStart = firstStart - 1;
+  var aPath = this.path;
 
+  //dupplicate page
+  var numP = this.numPages;
+  for (i = numP; i > 0 + firstStart; i = i - 1) {
+    this.insertPages(i - 1, aPath, i - 1, i - 1);
+  }
 
-	var firstStart = firstStart -1;
-	var aPath = this.path;
+  //crop page
+  var numP = this.numPages;
+  var left = true;
 
+  for (i = 0 + firstStart; i < numP; i++) {
+    if (left == true) {
+      var left = false;
+      var aRect = this.getPageBox("Trim", i);
+      var hoehe = aRect[2] - aRect[0];
+      var breite = aRect[1] - aRect[3];
 
-	//duppliziert jede seite
-    	var numP = this.numPages 
-    	for (i=numP;i>0+firstStart;i=i-1) {
-    	    this.insertPages (i-1, aPath, i-1,i-1);
-    	}
+      this.setPageBoxes("Trim", i, i, [aRect[0], aRect[1], aRect[2] - breite / 2, aRect[3]]);
 
-	//cropt nun die seiten richtig
-    	var numP = this.numPages 
-    	var left = true
+      var aRect = this.getPageBox("Trim", i);
+      this.setPageBoxes("Media", i, i, [aRect[0] - randpt, aRect[1] + randpt, aRect[2] + randpt, aRect[3] - randpt]);
+    } else {
+      var left = true;
+      var aRect = this.getPageBox("Trim", i);
+      var hoehe = aRect[2] - aRect[0];
+      var breite = aRect[1] - aRect[3];
 
-    	for (i=0 +firstStart;i<numP;i++) {
-    	    if (left==true) {
-				var left=false;
-				var aRect = this.getPageBox("Trim", i);
-				var hoehe = aRect[2] -aRect[0]
-				var breite = aRect[1] - aRect[3]
-				
-				this.setPageBoxes("Trim", i, i, [aRect[0], aRect[1], aRect[2]-breite/2, aRect[3]]);
-			
-				var aRect = this.getPageBox("Trim", i);
-				this.setPageBoxes("Media", i, i, [aRect[0]-randpt,aRect[1]+randpt,aRect[2]+randpt,aRect[3]-randpt]);
-			
-			} 
-			else
-			{
-				var left=true;
-				var aRect = this.getPageBox("Trim", i);
-				var hoehe = aRect[2] -aRect[0]
-				var breite = aRect[1] - aRect[3]
-				
-				this.setPageBoxes("Trim", i, i, [aRect[0]+breite/2,aRect[1],aRect[2],aRect[3]]);
-				
-				var aRect = this.getPageBox("Trim", i);
-				this.setPageBoxes("Media", i, i, [aRect[0]-randpt,aRect[1]+randpt,aRect[2]+randpt,aRect[3]-randpt]);
-			
-    		};
-   	 	}
+      this.setPageBoxes("Trim", i, i, [aRect[0] + breite / 2, aRect[1], aRect[2], aRect[3]]);
 
-	return ("Seiten wurden gesplittet.");
+      var aRect = this.getPageBox("Trim", i);
+      this.setPageBoxes("Media", i, i, [aRect[0] - randpt, aRect[1] + randpt, aRect[2] + randpt, aRect[3] - randpt]);
+    }
+  }
+
+  return "Seiten wurden gesplittet.";
 }
